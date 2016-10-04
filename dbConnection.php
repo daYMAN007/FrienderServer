f8b76b5c2c37a85e282dd8bde693211d3886fa63
simisos <simon-brun@bluewin.ch>
2016-10-04 20:32:28 +0200

    Deploy Script, Edin din code suckt ha alles gfixt

--- dbConnection.php
+++ dbConnection.php
@@ -1,5 +1,6 @@
 <?php
-//Verbindung zur Datenbank aufbauen
+// btw i ha mi asi gfühlt wo i fascht alles glöscht ha
+//Verbindung zur Datenbank aufbauen //damian:delete da?
 $BenutzerId;
 $Benutzername;
 $BenVorname;
@@ -10,51 +11,28 @@ $BenLatitude;
 $BenGeoTime;
 $BenPasswort;
 
+require_once 'dbconnect.php';
+mysqli_set_charset($db, 'utf8'); //nöd nötig wen nix mb4 (https://forums.digitalpoint.com/threads/mysql-collation-utf8-differences.2721197/)
+
 // Daten einfügen
-function DatenEinfuegen ($Benutzername, $BenVorname, $Nachname, $BenTelefonnummer, $BenLongitude, $BenLatitude, $BenGeoTime, $BenPasswort)
+function DatenEinfuegen ($Benutzername, $BenVorname, $BenNachname, $BenTelefonnummer, $BenLongitude, $BenLatitude, $BenGeoTime, $BenPasswort) //func name= Registrieren?
 {
-$db = @new mysqli('localhost', 'root', '', 'frienderdb');
-mysqli_set_charset('utf8');
-require_once('config.php');
-$link = mysqli_connect(MY_HOST, MY_USER, MY_PASSWORD, MY_DB);
-if(mysqli_connect_errno())
-{
-  echo "Sie besitzen leider keine Internetverbindung. Verbinden Sie sich bitte mit dem Internet.";
-  exit();
-}else{
-  // echo "Connection established.";
-}
-
-// Charset definieren
-mysqli_set_charset($link, 'utf8mb4');
-
+global $db;
 //Tabelle füllen
 $sql = "INSERT INTO tbenutzer (BenutzerId, BenNickname, BenVorname, BenNachname, BenTelefonnummer, BenLongitude, BenLatitude, BenGeoTime, BenPasswort) VALUES (NULL, '$Benutzername', '$BenVorname', '$BenNachname', '$BenTelefonnummer', '$BenLongitude', '$BenLatitude', '$BenGeoTime','$BenPasswort');";
-$sql = mysqli_query($link, $sql) or die ("Error message: " . mysqli_error($link));
+$sql = mysqli_query($db, $sql) or die ("Error message: " . mysqli_error($db));
 }
 
 // Daten auslesen
-function DatenAuslesen ()
+DatenEinfuegen("Hanswurst","Peter","Hansel","sfdsd",5,3,"now()","");
+DatenAuslesen(1000);
+function DatenAuslesen ($BenutzerId) 
 {
-  $db = @new mysqli('localhost', 'root', '', 'frienderdb');
-  mysqli_set_charset('utf8');
-  require_once('config.php');
-  $link = mysqli_connect(MY_HOST, MY_USER, MY_PASSWORD, MY_DB);
-  if(mysqli_connect_errno())
-  {
-    echo "Sie besitzen leider keine Internetverbindung. Verbinden Sie sich bitte mit dem Internet.";
-    exit();
-  }else{
-    // echo "Connection established.";
-  }
-
-  // Charset definieren
-  mysqli_set_charset($link, 'utf8mb4');
-
   //Tabelle auselsen
+  global $db;
   $sql = "SELECT * FROM tbenutzer WHERE BenutzerId = '$BenutzerId';";
-  $sql = mysqli_query($link, $sql) or die ("Error message: " . mysqli_error($link));
-  $user = $sql->query($sql)->fetch();
+  $result = mysqli_query($db, $sql) or die ("Error message: " . mysqli_error($db));
+  $user = mysqli_fetch_assoc($result);
   $BenutzerId = $user['BenutzerId'];
   $Benutzername = $user['BenNickname'];
   $BenVorname = $user['BenVorname'];
@@ -67,26 +45,16 @@ function DatenAuslesen ()
 }
 
 // Daten aktualisieren
-function DatenUpdaten ($BenutzerId, $Benutzername, $BenVorname, $BenNachname, $BenTelefonnummer, $BenLongitude, $BenLatitude, $BenGeoTime, $BenPasswort)
-$db = @new mysqli('localhost', 'root', '', 'frienderdb');
-mysqli_set_charset('utf8');
-require_once('config.php');
-$link = mysqli_connect(MY_HOST, MY_USER, MY_PASSWORD, MY_DB);
-if(mysqli_connect_errno())
+function PositionUpdate($BenutzerId, $Benutzername, $BenVorname, $BenNachname, $BenTelefonnummer, $BenLongitude, $BenLatitude, $BenGeoTime, $BenPasswort) //name gänderet
 {
-  echo "Sie besitzen leider keine Internetverbindung. Verbinden Sie sich bitte mit dem Internet.";
-  exit();
-}else{
-  // echo "Connection established.";
-}
 
 //Charset definieren
-mysqli_set_charset($link, 'utf8mb4');
+//mysqli_set_charset($db, 'utf8mb4'); //REDUNDANT
 
 //Tabelle updaten
 $sql = "UPDATE tbenutzer set (BenLongitude, BenLatitude, BenGeoTime) VALUES ('$BenLongitude', '$BenLatitude', '$BenGeoTime') where BenutzerId = '$BenutzerId';";
-$sql = mysqli_query($link, $sql) or die ("Error message: " . mysqli_error($link));
-
+$sql = mysqli_query($db, $sql) or die ("Error message: " . mysqli_error($db)); //maybe bessere name für variable? well du chunsch kei sql über maybe $reponse or sth like this
+}
 
 //To do
 //-Datenüberprüfung,
--- dbconnect.php
+++ dbconnect.php
@@ -1,4 +1,6 @@
 <?php
 
 require_once 'settings.php';
+
 $db = mysqli_connect("$dbhost", "$dbusername", "$dbpassword","$dbname")or die("Error " . mysqli_error($db));      
+global $db ;
